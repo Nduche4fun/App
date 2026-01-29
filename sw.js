d3593f1f583f025b35ccdcab47320b1cda25ad78
Nduche4fun <bryannduche790@gmail.com>
2026-01-29 21:36:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,16 +5,30 @@ const ASSETS = [
   './manifest.json'
 ];
 
-// Install event: Triggered when the user installs the PWA
+// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate event (THIS is the missing piece)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
-// Fetch event: Serve files from cache when offline
+// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
